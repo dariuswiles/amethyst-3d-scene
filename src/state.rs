02b5a00e524d5934a8ca7a416ccfb0ea74116b06
Darius Wiles <github@dariuswiles.com>
2021-03-25 09:42:24 -0700

    Add cube

--- src/state.rs
+++ src/state.rs
@@ -15,9 +15,9 @@ impl SimpleState for MyGameState {
     fn on_start(&mut self, state_data: StateData<'_, GameData<'_, '_>>) {
         init_camera(state_data.world);
         init_sphere(state_data.world);
+        init_cube(state_data.world);
         initialize_light(state_data.world);
     }
-
 }
 
 /// Creates a camera entity in the `world`.
@@ -27,14 +27,14 @@ fn init_camera(world: &mut World) {
 
     world
         .create_entity()
-        .with(Camera::standard_3d(1000.0, 800.0))  // width, height are dimensions of window
+        .with(Camera::standard_3d(1000.0, 800.0))  // width, height must be dimensions of window to avoid distortion
         .with(transform)
         .build();
 }
 
 fn init_sphere(world: &mut World) {
     let mut transform = Transform::default();
-    transform.set_translation_xyz(0.0, 0.0, 0.0);
+    transform.set_translation_xyz(3.0, 3.0, 0.0);
 
     let mesh = world.exec(|loader: AssetLoaderSystemData<'_, Mesh>| {
         loader.load_from_data(
@@ -63,6 +63,51 @@ fn init_sphere(world: &mut World) {
         .with(transform)
         .build();
 }
+
+
+fn init_cube(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(-3.0, -2.0, 0.0);
+
+    let mesh = world.exec(|loader: AssetLoaderSystemData<'_, Mesh>| {
+        loader.load_from_data(
+            shape::Shape::Cube
+                .generate::<(Vec<rendy::mesh::Position>, Vec<rendy::mesh::Normal>, Vec<rendy::mesh::Tangent>, Vec<rendy::mesh::TexCoord>)>(None)
+                .into(),
+            (),
+        )
+    });
+
+    let material_defaults = world.read_resource::<MaterialDefaults>().0.clone();
+    let material = world.exec(|loader: AssetLoaderSystemData<'_, Material>| {
+            loader.load_from_data(
+                Material {
+                    ..material_defaults
+                },
+                (),
+            )
+        },
+    );
+
+    world
+        .create_entity()
+        .with(mesh)
+        .with(material)
+        .with(transform)
+        .build();
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 fn initialize_light(world: &mut World) {
