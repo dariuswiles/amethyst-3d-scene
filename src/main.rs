f3482b1b5417452f5fe2cd2257c0c82ef4c5bdd9
Darius Wiles <github@dariuswiles.com>
2021-03-24 17:55:09 -0700

    Create repo

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use amethyst::{
+    core::transform::{TransformBundle},
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderToWindow, RenderPbr3D},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+    window::DisplayConfig,
+};
+
+mod state;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let resources = app_root.join("assets");
+
+    let mut display_config = DisplayConfig::default();
+    display_config.title = "My first Amethyst 3D program".to_string();
+    display_config.dimensions = Some((1000, 800));
+
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(InputBundle::<StringBindings>::new())?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config(display_config)
+                        .with_clear([0.3, 0.7, 0.2, 1.0]),
+                )
+                .with_plugin(RenderUi::default())
+                .with_plugin(RenderPbr3D::default()),
+        )?;
+
+    let mut game = Application::new(resources, state::MyGameState, game_data)?;
+    game.run();
+
+    Ok(())
+}
---
+++ src/state.rs
@@ -0,0 +1,83 @@
+use amethyst::{
+    assets::{AssetLoaderSystemData},
+    core::transform::{Transform},
+    prelude::*,
+    renderer::{Camera, light, Material, MaterialDefaults, Mesh, palette, rendy, shape,
+    },
+    //window::ScreenDimensions,
+};
+
+/// A dummy game state that shows 3 sprites.
+pub struct MyGameState;
+
+impl SimpleState for MyGameState {
+
+    fn on_start(&mut self, state_data: StateData<'_, GameData<'_, '_>>) {
+        init_camera(state_data.world);
+        init_sphere(state_data.world);
+        initialize_light(state_data.world);
+    }
+
+}
+
+/// Creates a camera entity in the `world`.
+fn init_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(0.0, 0.0, 10.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_3d(1000.0, 800.0))  // width, height are dimensions of window
+        .with(transform)
+        .build();
+}
+
+fn init_sphere(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(0.0, 0.0, 0.0);
+
+    let mesh = world.exec(|loader: AssetLoaderSystemData<'_, Mesh>| {
+        loader.load_from_data(
+            shape::Shape::Sphere(100, 100)
+                .generate::<(Vec<rendy::mesh::Position>, Vec<rendy::mesh::Normal>, Vec<rendy::mesh::Tangent>, Vec<rendy::mesh::TexCoord>)>(None)
+                .into(),
+            (),
+        )
+    });
+
+    let material_defaults = world.read_resource::<MaterialDefaults>().0.clone();
+    let material = world.exec(|loader: AssetLoaderSystemData<'_, Material>| {
+            loader.load_from_data(
+                Material {
+                    ..material_defaults
+                },
+                (),
+            )
+        },
+    );
+
+    world
+        .create_entity()
+        .with(mesh)
+        .with(material)
+        .with(transform)
+        .build();
+}
+
+
+fn initialize_light(world: &mut World) {
+    let light: light::Light = light::PointLight {
+        intensity: 10.0,
+        color: palette::rgb::Rgb::new(1.0, 1.0, 1.0),
+        ..light::PointLight::default()
+    }.into();
+
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(5.0, 5.0, 20.0);
+
+    world
+        .create_entity()
+        .with(light)
+        .with(transform)
+        .build();
+}
