6f13585e1e8be935beaf2613c3cbdd12a47fc87f
Darius Wiles <github@dariuswiles.com>
2021-04-27 12:34:03 -0700

    Implement camera movement (though needs work)

--- src/system.rs
+++ src/system.rs
@@ -2,6 +2,7 @@
 use amethyst::{
     core::{SystemDesc},
     core::transform::{Transform, TransformBundle},
+    core::math::Vector3,
     derive::SystemDesc,
     ecs::{Join, Read, ReadStorage, System, SystemData, World, WriteStorage},
     input::{InputBundle, InputHandler, StringBindings},
@@ -17,7 +18,6 @@ use amethyst::{
 };
 
 
-#[derive(SystemDesc)]
 pub struct MyMoveSystem;
 
 impl<'a> System<'a> for MyMoveSystem {
@@ -27,24 +27,32 @@ impl<'a> System<'a> for MyMoveSystem {
         Read<'a, InputHandler<StringBindings>>,
     );
 
-//     fn run(&mut self, (mut transforms, paddles, input): Self::SystemData) {
     fn run(&mut self, (mut transforms, camera, input): Self::SystemData) {
 
         for (camera, transform) in (&camera, &mut transforms).join() {
 
-            let i: &InputHandler<StringBindings> = &*input;
+//             let i: &InputHandler<StringBindings> = &*input;
 
-            if let Some(v) = i.axis_value("vertical") {
+            let mut x = 0.0;
+            let mut z = 0.0;
+
+
+            if let Some(v) = input.axis_value("vertical") {
                 if v != 0.0 {
                     println!("Movement detected on vertical axis, value is {}", v);
+                    z = v;
                 }
             }
 
-            if let Some(v) = i.axis_value("horizontal") {
-                if v != 0.0 {
-                    println!("Movement detected on horizontal axis, value is {}", v);
+            if let Some(h) = input.axis_value("horizontal") {
+                if h != 0.0 {
+                    println!("Movement detected on horizontal axis, value is {}", h);
+                    x = h;
                 }
             }
+
+            transform.append_translation(Vector3::new(x, 0.0, z));
+
         }
     }
 }
